bfd07b5e76ff0cada11e383a5c4b254d4ac7f588
Keep Zen <keep.in.zen@pm.me>
2021-12-06 12:43:23 +0800

    first publish

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule M.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      licenses: "MIT",
+      description: "Reverse match for compose with |>",
+      package: package(),
       deps: deps()
     ]
   end
@@ -18,9 +21,18 @@ defmodule M.MixProject do
     ]
   end
 
+  defp package do
+    [
+      name: "reverse_match",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/KeepZen/reverse_match"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
